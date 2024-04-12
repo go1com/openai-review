e1be5dfb8ac944db76e31e432a2896fb6776ce82
Sang Le <sang@go1.com>
2024-04-12 15:31:11 +1000

    Update git fetch before run git diff

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import * as core from "@actions/core";
-import { computeDiff, showColorDiff } from './diff';
+// import { computeDiff, showColorDiff } from './diff';
 import { AzureOpenAIExec } from './azure-openai';
 
 const main = async (): Promise<void> => {
-  core.startGroup('diff');
-  await showColorDiff(core.getInput("base"), core.getInput("head"));
-  core.endGroup();
 
-  const diffs = await computeDiff(core.getInput("base"), core.getInput("head"));
-  if (diffs.length === 0) {
-    core.info("No diff");
-    core.setOutput("text", "No diff");
-  }
+  const diffs = core.getInput("diff");
 
-  const text = await AzureOpenAIExec("Write a description for this git diff: \n ${diffs.map(diff => diff.content).join('\n')}");
+  const text = await AzureOpenAIExec(`Write a description for this git diff: \n ${diffs}`);
   // The output of this action is the text from OpenAI trimmed and escaped
   core.setOutput(
     "text",
